dd01b51e03b89160806b26e479f8d8fdf3dc2085
Francesca <54044515+fsilvabel@users.noreply.github.com>
2025-02-12 15:44:36 -0500

    Create 10_currency.py

---
+++ 2-variables/10_currency.py
@@ -0,0 +1,8 @@
+#Currency
+#Codedex
+pesos = int(input("What do you have left in pesos? "))
+soles = int(input("What do you have left in soles? "))
+reais = int(input("What do you have left in reais? "))
+
+total = (pesos*0.00090)+(soles*3.70)+(reais*0.172201)
+print(total)
