d3f9247c4d3a8874e33a7c1b7368ab03c8faa46f
Francesca <54044515+fsilvabel@users.noreply.github.com>
2025-02-11 22:47:10 -0500

    Create 05_snail_mail.py

---
+++ 01-hello-world/05_snail_mail.py
@@ -0,0 +1,11 @@
+# Snail Mail
+# Codedex
+
+print('01/27/2025')
+print('Im feeling great to have the oportunity to learn Python')
+print('I want to accomplish a new skill')
+print('Your are doing great')
+print('😊')
+
+# OMG we're so proud of you! (╥﹏╥)
+print('Congrats! You\'ve made it to the end of chapter one! 🫶')
